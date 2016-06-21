6e2e616cca3795ed1af8361f805e633f2218e65d
Simon <noisyboiler@googlemail.com>
2016-06-21 18:39:19 +0100

    remove silly assertions

--- wampy/rpc.py
+++ wampy/rpc.py
@@ -38,10 +38,6 @@ class RpcProxy:
 
 def register_rpc(*args, **kwargs):
     assert isinstance(args[0], types.FunctionType)
-    # don't support (yet) entrypoints taking args and kwargs
-    assert len(args) == 1
-    assert kwargs == {}
-
     wrapped = args[0]
 
     def decorator(fn, *args, **kwargs):
