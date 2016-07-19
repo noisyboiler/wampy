5db2ea0c75011a1311629fe3cdfde4a93db0f17a
Simon <noisyboiler@googlemail.com>
2016-07-19 14:25:03 +0200

    remove dead weight

--- test/integration/test_connections.py
+++ test/integration/test_connections.py
@@ -1,30 +1,6 @@
 from mock import ANY
 
-from wampy.networking.connections.tcp import TCPConnection
-from wampy.networking.connections.http import HttpConnection
-from wampy.networking.connections.wamp import WampConnection
-
-
-def test_remote_tcp_crossbar_connection():
-    connection = TCPConnection(host='wampy.online', port=8082)
-    connection.connect()
-
-
-def test_remote_http_crossbar_connection():
-    connection = HttpConnection(host='wampy.online', port=8082)
-    connection.connect()
-
-    assert connection.status == 404  # yes, this is the expected behaviour!
-    assert connection.headers == {
-        'status': 404,
-        'content-length': ANY,
-        'server': 'crossbar/0.13.2',
-        'date': ANY,
-        'status_info': [
-            'HTTP/1.1', '404', 'Not Found'
-        ],
-        'content-type': 'text/html'
-    }
+from wampy.networking.connection import WampConnection
 
 
 def test_remote_host_wamp_connection():
