a70140d0022b128c5ab4a4a4553fc84543e70092
Simon <noisyboiler@googlemail.com>
2016-09-30 05:00:21 +0100

    protocol examples as tests

--- test/unit/test_subscriptions.py
+++ test/unit/test_subscriptions.py
@@ -5,24 +5,27 @@ from wampy.entrypoints import subscribe
 from test.helpers import assert_stops_raising
 
 
-
-class MetaSubscribingClient(Peer):
+class MetaClient(Peer):
 
     call_count = 0
 
     @subscribe(topic="wamp.subscription.on_create")
-    def on_create_handler(self, **kwargs):
+    def on_create_handler(self, *args, **kwargs):
         self.call_count += 1
 
-    @subscribe(topic="wamp.subscription.on_subscribe")
-    def on_subscribe_handler(self, **kwargs):
-        self.call_count += 1
 
+@pytest.yield_fixture
+def publisher_without_subscribers(router):
+    client = Peer(name="publisher")
+    client.start()
+    client.publish(topic="foo", message="bar")
+    yield client
+    client.stop()
 
 
 @pytest.yield_fixture
 def meta_subscriber(router):
-    peer = MetaSubscribingClient(name="subscriber")
+    peer = MetaClient(name="subscriber")
 
     peer.start()
     yield peer
@@ -31,14 +34,20 @@ def meta_subscriber(router):
 
 class TestMetaEvents:
 
-    def test_subscription_on_create(self, meta_subscriber):
-        assert meta_subscriber.call_count == 0
+    def test_subscription_on_create(
+            self, publisher_without_subscribers, meta_subscriber
+    ):
 
-        client = Peer(name="publisher")
-        with client:
-            client.publish(topic="foo", message="bar")
+        class ClientPeer(Peer):
+            @subscribe(topic="foo")
+            def on_subscribe_handler(self, **kwargs):
+                pass
 
-        def check_call_count():
-            assert meta_subscriber.call_count == 1
+        subscriber = ClientPeer(name="subscriber")
 
-        assert_stops_raising(check_call_count)
+        with subscriber:
+
+            def check_call_count():
+                assert meta_subscriber.call_count == 1
+
+            assert_stops_raising(check_call_count)
