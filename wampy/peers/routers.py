5bdfa8e4f220172657ae0083fa723b00e39b2137
Simon <noisyboiler@googlemail.com>
2017-01-15 17:11:13 +0000

    bump version for Router params change

--- wampy/peers/routers.py
+++ wampy/peers/routers.py
@@ -5,14 +5,20 @@ from wampy.constants import DEFAULT_REALM, DEFAULT_ROLES
 class Router(object):
     """Base class for any Router implementation"""
 
+    def __init__(
+            self, host=DEFAULT_HOST, port=DEFAULT_PORT,
+    ):
+        self.host = host
+        self.port = port
+
 
 class Crossbar(Router):
 
     def __init__(
             self, host=DEFAULT_HOST, port=DEFAULT_PORT,
-            realms=[DEFAULT_REALM], roles=DEFAULT_ROLES
+            realms=None, roles=None
     ):
-        self.host = host
-        self.port = port
-        self.reals = realms or []
-        self.roles = roles
+        super(Crossbar, self).__init__(host, port)
+
+        self.realms = realms or [DEFAULT_REALM]
+        self.roles = roles or DEFAULT_ROLES
