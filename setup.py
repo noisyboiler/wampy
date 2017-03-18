fe2cb4be363da6ba913a1fb321eb7669649d5c4c
Simon <noisyboiler@googlemail.com>
2017-03-18 22:12:41 +0000

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst')) as f:
 
 setup(
     name='wampy',
-    version='0.7.8',
+    version='0.8.0',
     description='WAMP RPC and Pub/Sub for python apps and microservices',
     long_description=long_description,
     url='https://github.com/noisyboiler/wampy',
