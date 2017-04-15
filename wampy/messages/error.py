60b4d6a294a7fcad69e96ed9a76a29e842ce75a4
Simon <noisyboiler@googlemail.com>
2017-04-15 10:29:58 +0100

    fix missing params on Error message

--- wampy/messages/error.py
+++ wampy/messages/error.py
@@ -9,7 +9,10 @@ logger = logging.getLogger('wampy.messages.error')
 class Error(Message):
     WAMP_CODE = 8
 
-    def __init__(self, wamp_code, request_type, request_id, details, error):
+    def __init__(
+            self, wamp_code, request_type, request_id, details, error,
+            args_list=None, kwargs_dict=None
+    ):
         """ Error reply sent by a Peer as an error response to
         different kinds of requests.
 
@@ -29,6 +32,8 @@ class Error(Message):
         self.request_id = request_id
         self.details = details
         self.error = error
+        self.args_list = args_list
+        self.kwargs_dict = kwargs_dict
 
     def process(self, client=None):
         logger.error(self.error)
