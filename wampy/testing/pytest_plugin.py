10aa44446ce4c936f4583870d6a5099d695d41dc
Simon <noisyboiler@googlemail.com>
2017-07-11 12:26:09 +0200

    clearer Crossbar log messages

--- wampy/testing/pytest_plugin.py
+++ wampy/testing/pytest_plugin.py
@@ -121,7 +121,7 @@ def find_processes(process_name):
 
 def get_process_ids():
     output = find_processes("crossbar")
-    pids = [o for o in output.decode().split('\n') if o]    
+    pids = [o for o in output.decode().split('\n') if o]
     return pids
 
 
@@ -136,21 +136,24 @@ def kill_crossbar(try_again=True):
         pid = int(pid_as_str)
 
         try:
-            os.waitpid(pid, options=os.WNOHANG)
-        except OSError:
-            pass
-
-        try:
             logger.warning("OS sending SIGTERM to crossbar pid: %s", pid)
             os.kill(pid, signal.SIGTERM)
         except OSError:
-            logger.error("Failed to terminate router process again: %s", pid)
+            logger.exception(
+                "Failed to terminate router process: %s", pid)
+
+            try:
+                os.waitpid(pid, options=os.WNOHANG)
+            except OSError:
+                pass
+
             try:
                 os.kill(int(pid), signal.SIGKILL)
             except Exception as exc:
                 if "No such process" in str(exc):
-                    return
-                logger.exception("Failed to shutdown router")
+                    continue
+                logger.exception(
+                    "Failed again to terminate router process: %s", pid)
 
     pids = get_process_ids()
     if pids and try_again is True:
