aa80c152cc0b3e7b821f0edefd23a7d4ed34b0d7
Simon <noisyboiler@googlemail.com>
2018-03-10 15:49:01 +0000

    use gevent thread to run server

--- test/transports/test_websockets.py
+++ test/transports/test_websockets.py
@@ -1,27 +1,33 @@
-from threading import Thread
-
 import pytest
+import gevent
+from gevent import Greenlet
 from geventwebsocket import WebSocketServer, Resource
 
 from wampy.transports.websocket.connection import WebSocket
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture
 def server():
     s = WebSocketServer(
-        ('0.0.0.0', 8000),
+        ('0.0.0.0', 8001),
         Resource([]),
         debug=False,
     )
-
-    server_thread = Thread(target=s.serve_forever)
-    server_thread.daemon = True
-    server_thread.start()
+    thread = Greenlet.spawn(s.serve_forever)
     yield s
-    s.stop()
+    thread.kill()
 
 
 def test_websocket_connects_to_server(server):
-    websocket = WebSocket(server_url='ws://0.0.0.0:8000')
+    websocket = WebSocket(server_url='ws://0.0.0.0:8001')
     websocket.connect()
     websocket.disconnect()
+
+
+def __test_send_ping(server):
+    websocket = WebSocket(server_url='ws://0.0.0.0:8000')
+    websocket.connect()
+
+
+
+
