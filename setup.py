51b123350f7cb260b402c4993496cc006fadef7d
Simon <noisyboiler@googlemail.com>
2020-08-28 11:19:35 +0100

    fix bad classifier

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7.3',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
     keywords='WAMP RPC',
